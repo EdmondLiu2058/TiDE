76032a6ce026733509e48eb711de24b2000f7a50
Edmond_Liu <jl2058@hw.ac.uk>
2023-07-26 00:19:33 +0100

    split job.sh

--- job_serial.sh
+++ job_serial.sh
@@ -9,35 +9,19 @@
 #SBATCH -o job-%j.output
 #SBATCH -e job-%j.error
 ## Job name
-#SBATCH -J gpu-test
+#SBATCH -J TiDE_Train
 ## Run time: "hours:minutes:seconds", "days-hours"
 #SBATCH --time=24:05:00
-## Memory limit (in megabytes). Total --mem or amount per cpu --mem-per-cpu
-#SBATCH --mem-per-cpu=102400
-## GPU requirements
-#SBATCH --gres gpu:1
+## Memory limit (in megabytes) 100GB
+#SBATCH --mem=102400
 ## Specify partition
-#SBATCH -p gpu
+#SBATCH -p nodes
 
 ################# Part-2 Shell script ####################
 #===============================
 #  Activate Flight Environment
 #-------------------------------
 source "${flight_ROOT:-/opt/flight}"/etc/setup.sh
-
-#==============================
-#  Activate Package Ecosystem
-#------------------------------
-# e.g.:
-# Load the OpenMPI module for access to `mpirun` command
-flight env activate conda
-# flight env activate gridware
-# module load mpi/openmpi
-
-# if ! command -v mpirun &>/dev/null; then
-#     echo "No mpirun command found, ensure that a version of MPI is installed and available in PATH" >&2
-#     exit 1
-# fi
 
 #===========================
 #  Create results directory
@@ -57,8 +41,8 @@ echo "Executing job commands, current working directory is $(pwd)"
 
 echo "Hello, dmog" > $RESULTS_DIR/test.output
 echo "This is an example job. It ran on `hostname -s` (as `whoami`)." >> $RESULTS_DIR/test.output
-echo "I was allocated the following GPU devices: $CUDA_VISIBLE_DEVICES" >> $RESULTS_DIR/test.output
 echo "Output file has been generated, please check $RESULTS_DIR/test.output"
+
 for horizon in 96 
   do
     python3 -u -m train \
@@ -80,4 +64,3 @@ for horizon in 96
   done
 
 echo "finished"
-  
